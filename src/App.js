d840ad22c1c8193c0b9c1561a73b100589d7dfb8
NicolasCarniello <nicolascarniello1@gmail.com>
2023-03-03 17:13:19 -0300

    Trip Project

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+
+const App1 = () => {
+  const [loading, setLoading] = useState(false)
+  const [tours, setTours] = useState([])
+  const url = 'https://course-api.com/react-tours-project'
+
+  const fetchTours = async () => {
+    try {
+      setLoading(true)
+      const response = await fetch(url)
+      const tours = await response.json()
+      setLoading(false)
+      setTours(tours)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+
+  const removeTour = (id) => {
+    const toursFiltered = tours.filter((tour) => tour.id !== id)
+    setTours(toursFiltered)
+  }
+
+  useEffect(() => {
+    fetchTours()
+  }, [])
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            Refresh tours
+          </button>
+        </div>
+      </main>
+    )
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
+}
+
+export default App1
